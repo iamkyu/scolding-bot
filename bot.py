273013253196322157c4c879042a396bba03b4f5
kjnam <kyujnam@gmail.com>
2017-01-18 22:27:55 +0900

    init

---
+++ bot.py
@@ -0,0 +1,48 @@
+import slack as slack
+from slacker import Slacker
+from ConfigParser import SafeConfigParser
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+import datetime
+import pytz
+import random
+
+sched = BlockingScheduler()
+timezone = pytz.timezone('Asia/Seoul')
+channels = ['#random']
+
+
+def get_slack_token():
+    parser = SafeConfigParser()
+    parser.read('slack.ini')
+    return parser.get('slack', 'token')
+
+
+def post_to_channel(message, idx):
+    slack.chat.post_message(channels[idx], message, as_user=True)
+
+
+def get_today_of_week():
+    return datetime.datetime.isoweekday(datetime.datetime.now(timezone))
+
+
+def get_sending_time(day):
+    if day >= 6:
+        hour_from = 13
+        hour_to = 21
+    else:
+        hour_from = 19
+        hour_to = 22
+
+    return random.randrange(hour_from, hour_to)
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=get_sending_time(get_today_of_week()))
+def main():
+    slacker = Slacker(get_slack_token())
+    slacker.chat.post_message(channels[0], 'message', as_user=True)
+
+
+if __name__ == '__main__':
+    # sched.start()
+    main()
---
+++ readme.md
@@ -0,0 +1,18 @@
+# scolding-bot
+
+## 봇 제작!
+
+파이썬 패키지 관리를 위한 pip  설치
+
+```shell
+$ sudo easy_install pip
+```
+
+
+
+# 참고
+
+- [slacker로 slack bot 만들기](https://hyesun03.github.io/2016/10/08/slackbot/)
+- [일일커밋 알림봇 개발기](https://mingrammer.com/dev-commit-alarm-bot)
+- [lazy-check-bot](https://github.com/maxtortime/lazy-check-bot)
+- [Scheduled Jobs with Custom Clock Processes in Python with APScheduler](https://devcenter.heroku.com/articles/clock-processes-python)
