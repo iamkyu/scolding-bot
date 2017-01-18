ea8aef08f58017867ee3807f842c3745fcd53cb6
kjnam <kyujnam@gmail.com>
2017-01-18 23:32:39 +0900

    chore: 봇 메시지 리스트 추가

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from slacker import Slacker
 from ConfigParser import SafeConfigParser
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -9,16 +10,19 @@ import random
 sched = BlockingScheduler()
 timezone = pytz.timezone('Asia/Seoul')
 channels = ['#random']
+messages = [
+    u'>\n..? 자니? 스터디 과제는 다했구? ( ͡° ͜ʖ ͡°)',
+    u'>\n웅? 스터디 복습은 다 하고 티비 보고 있는 거야? ( ͡° ͜ʖ ͡°)ㅎ',
+    u'>\n공부해야한다.\n공부해야한다.\n공부해야한다',
+    u'>\n하라는 스터디 과제는 안하고ㅎ 넝담~ㅎ',
+    u'>\n심심한데 조쉬롱 아저씨 라이브 코딩이나 볼까ㅎ\nhttps://www.youtube.com/watch?v=fxB0tVnAi0I'
+]
 
 
 def get_slack_token():
     parser = SafeConfigParser()
     parser.read('slack.ini')
     return parser.get('slack', 'token')
-
-
-def post_to_channel(message, idx):
-    slack.chat.post_message(channels[idx], message, as_user=True)
 
 
 def get_today_of_week():
@@ -39,7 +43,7 @@ def get_sending_time(day):
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=get_sending_time(get_today_of_week()))
 def main():
     slacker = Slacker(get_slack_token())
-    slacker.chat.post_message(channels[0], 'message', as_user=True)
+    slacker.chat.post_message(channels[0], random.choice(messages), as_user=True)
 
 
 if __name__ == '__main__':
