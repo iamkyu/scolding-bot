a12da706395e7182ca25e29a36d575dd49dc5e18
kjnam <kyujnam@gmail.com>
2017-01-19 21:08:51 +0900

    refactor: 코드개선
    - 메세지 목록을.ini에서 관리하도록 변경
    - 일부 함수 개선

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+
 from slacker import Slacker
 from ConfigParser import SafeConfigParser
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -6,23 +7,24 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import datetime
 import pytz
 import random
+import codecs
+import json
 
 sched = BlockingScheduler()
 timezone = pytz.timezone('Asia/Seoul')
+parser = SafeConfigParser()
+parser.readfp(codecs.open('slack.ini', 'r', 'utf8'))
+
 channels = ['#random']
-messages = [
-    u'>\n..? 자니? 스터디 과제는 다했구? ( ͡° ͜ʖ ͡°)',
-    u'>\n웅? 스터디 복습은 다 하고 티비 보고 있는 거야? ( ͡° ͜ʖ ͡°)ㅎ',
-    u'>\n공부해야한다.\n공부해야한다.\n공부해야한다',
-    u'>\n하라는 스터디 과제는 안하고ㅎ 넝담~ㅎ',
-    u'>\n심심한데 조쉬롱 아저씨 라이브 코딩이나 볼까ㅎ\nhttps://www.youtube.com/watch?v=fxB0tVnAi0I'
-]
 
 
 def get_slack_token():
-    parser = SafeConfigParser()
-    parser.read('slack.ini')
     return parser.get('slack', 'token')
+
+
+def get_message():
+    messages = json.loads(parser.get('data', 'messages'))
+    return messages[random.randrange(0, len(messages))]
 
 
 def get_today_of_week():
@@ -30,21 +32,15 @@ def get_today_of_week():
 
 
 def get_sending_time(day):
-    if day >= 6:
-        hour_from = 13
-        hour_to = 21
-    else:
-        hour_from = 19
-        hour_to = 22
-
+    hour_from = 13 if day >= 6 else 19
+    hour_to = 22
     return random.randrange(hour_from, hour_to)
 
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=get_sending_time(get_today_of_week()))
 def main():
     slacker = Slacker(get_slack_token())
-    slacker.chat.post_message(channels[0], random.choice(messages), as_user=True)
-
+    slacker.chat.post_message(channels[0], get_message(), as_user=True)
 
 if __name__ == '__main__':
     sched.start()
