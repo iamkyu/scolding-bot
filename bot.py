d7d22b644f7ebb37806c14e4a51cedb14ccfd081
Kyujin Nam <kyujin.nam@woowahan.com>
2017-01-24 09:33:18 +0900

    fix: 같은 메세지를 연속해서 보내지 않도록 임시처리

--- bot.py
+++ bot.py
@@ -10,12 +10,14 @@ import random
 import codecs
 import json
 
+logging.basicConfig()
 sched = BlockingScheduler()
 timezone = pytz.timezone('Asia/Seoul')
 parser = SafeConfigParser()
 parser.readfp(codecs.open('slack.ini', 'r', 'utf8'))
 
 channels = ['#random']
+last_send_msg_idx = 0
 
 
 def get_slack_token():
@@ -23,8 +25,14 @@ def get_slack_token():
 
 
 def get_message():
+    global last_send_msg_idx
     messages = json.loads(parser.get('data', 'messages'))
-    return messages[random.randrange(0, len(messages))]
+    while True:
+        rand_msg_idx = random.randrange(0, len(messages))
+        if rand_msg_idx != last_send_msg_idx:
+            last_send_msg_idx = rand_msg_idx
+            break
+    return messages[rand_msg_idx]
 
 
 def get_today_of_week():
@@ -35,7 +43,8 @@ def get_sending_time(day):
     is_weekend = day >= 6
     hour_from = 13 if is_weekend else 19
     hour_to = 22
-    return random.randrange(hour_from, hour_to)
+    sending_time = random.randrange(hour_from, hour_to)
+    return sending_time
 
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=get_sending_time(get_today_of_week()))
